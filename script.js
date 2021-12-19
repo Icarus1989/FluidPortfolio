72802d5bddd8b1729b672614497f71ffc6b5ca57
Icarus1989 <53438706+Icarus1989@users.noreply.github.com>
2021-12-19 17:31:13 +0100

    testing circle progress bar made with conic and radial gradient

--- script.js
+++ script.js
@@ -23,9 +23,29 @@ mainElement.addEventListener('scroll', () => {
         }
       }, 500);
     }).then(() => {
+      console.log('then...')
+      // for (let circle of circles) {
+      //   let canvasContainer = document.createElement('div');
+      //   canvasContainer.style.position = "absolute";
+      //   canvasContainer.style.top = 0;
+      //   canvasContainer.style.left = 0;
+      //   canvasContainer.style.zIndex = 0;
+      //   circle.append(canvasContainer);
+
+      //   let canvas = function (cnv) {
+      //     cnv.setup = function () {
+      //       cnv.createCanvas(circle.clientWidth, circle.clientHeight);
+      //       cnv.background(220);
+      //     }
+      //     cnv.draw = function () {
+
+      //     }
+      //   }
+      //   new p5(canvas, canvasContainer);
+      // }
       // setTimeout(() => {
       //   for (let i = 0; i < circles.length; i++) {
-      console.log('log');
+      // console.log('log');
       // circles[0].style.background = `conic-gradient(from 270deg at 50% 20%, transparent, 180deg, white 180deg 181deg, rgb(12, 61, 248) 181deg 360deg)`;
       // circles[0].style.backgroundPosition = `50% 20%`;
       // }
